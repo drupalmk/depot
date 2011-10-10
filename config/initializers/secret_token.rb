3f2f78bb0b04f0a095c9049ba8db0511923df4b3
drupalmk <leisikkeram@gmail.com>
2011-10-10 14:46:07 +0200

    Depot Scaffol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '72c7f6cd81bc9cdccce52aa4064cc2b470f091190b4e7ad915b2082c30b1a8868092338e33313c98741e1c0524ee880860dae7d65dfa6e50c8fb42ab75399776'
